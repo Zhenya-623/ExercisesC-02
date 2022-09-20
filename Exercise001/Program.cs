ff5ab751f44b481e4427d5578c9a534503404509
Евгений Политыко <Zhenya-623@internet.ru>
2022-09-20 21:26:01 +0300

    added exercises001

---
+++ Exercise001/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+int num = new Random().Next(100, 1000);
+
+int FindDigit(int num)
+{
+    num = new Random().Next(100, 1000);
+    Console.WriteLine(num);
+    int result = num % 100;
+    result = result / 10;
+    return result;
+}
+
+Console.WriteLine(FindDigit(num));
