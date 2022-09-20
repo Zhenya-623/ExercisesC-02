d514adfd3c7503781ab9dfa126ec47ebff121369
Евгений Политыко <Zhenya-623@internet.ru>
2022-09-21 00:16:19 +0300

    added Exercises003

---
+++ Exercises003/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число, обозначающее день недели");
+    string numberStr = Console.ReadLine();
+    int number = int.Parse(numberStr);
+    return number;
+}
+
+int number = GetNumber();
+
+if (number == 6 || number == 7)
+    Console.WriteLine("да");
+else
+if (number >= 1 && number <= 5)
+    Console.WriteLine("нет");
+else
+    Console.WriteLine("Значение не верное");
+
